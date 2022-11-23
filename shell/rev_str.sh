44d041bba1f552f1f12435e602c34550ffd1b40d
Shreyas Manwadkar <77020179+ShreyasManwadkar@users.noreply.github.com>
2022-11-23 13:37:01 +0545

    Update rev_str.sh

--- shell/rev_str.sh
+++ shell/rev_str.sh
@@ -1,22 +1,37 @@
-
-// reverse a string using shell script
-// reverse a string is in linux and unix
- 
-#!/ bin / bash
-// reading a string
-// using via user input
-read - p "Enter string:" string
-    // getting the length of given string
-    len
-    = $
-{
-    #string
-}
-// looping for reversing a string
-// initialize i=len-1 for reversing a string and run till i=0
-// printing in the reverse order of the given string
-for ((i = $len - 1; i >= 0; i--))
-    do
-    // "${string:$i:1}"extract single character from string.
-    reverse = "$reverse${string:$i:1}" done
-        echo "$reverse"
+echo "Enter String:"
+read str
+echo "Enter 1 to reverse $str"
+echo "Enter 2 to check wheather $str if palindrome or not:"
+echo "Enter your choice:"
+read choice
+case $choice in
+1)
+ g=${#str}
+ i=$(($g - 1))
+ reverse=""
+ while [ $i -ge 0 ]
+ do
+   reverse=$reverse${str:$i:1}
+   i=$(($i -1))
+ done 
+ echo "Reverse of the  $str is :- $reverse" ;;
+     
+2) 
+ g=${#str}
+ i=$(($g - 1))
+ reverse=""
+ while [ $i -ge 0 ]
+ do
+   reverse=$reverse${str:$i:1}
+   i=$(($i -1))
+ done 
+ echo "Reverse of the  $str is :- $reverse"
+ if [ $str == $reverse ]
+ then
+     echo "$str is Palindrome"
+ else
+     echo "$str is not a Palindrome"
+ fi;;
+*)
+ echo"Enter valid choice";;  
+esac
